c010bc3f26bf025f8225afec498c2c8c3c97d397
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-06-09 12:57:10 +0100

    Added more test case for two number sum

--- Array/two_sumCase.py
+++ Array/two_sumCase.py
@@ -6,7 +6,8 @@ class TestProgram(unittest.TestCase):
         self.assertEqual(twoSum.twoNumberSum([-4,-1,1,3,5,6,8,11], 13),[5,8])
     def test_case_2(self):
         self.assertEqual(twoSum.twoNumSum([-4,-1,1,3,5,6,8,11], 13),[5,8])
-
+    
+   
     
 
 if __name__ == '__main__':
